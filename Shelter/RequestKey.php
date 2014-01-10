2c9efd87278d1f0f13efd01dfaf52a2327dae476
VladaHejda <hejda@vspdata.cz>
2014-01-10 14:58:03 +0100

    RequestKey

--- Shelter/RequestKey.php
+++ Shelter/RequestKey.php
@@ -10,5 +10,29 @@ class RequestKey implements IRequestKey
 	 */
 	public function getKey()
 	{
+		$input = $extra = '';
+
+		// CLI
+		if ('cli' === PHP_SAPI) {
+			$extra = implode(' ', array_slice($_SERVER['argv'], 1));
+		}
+
+		if (isset($_SERVER['REQUEST_URI'])) {
+			$input = $_SERVER['REQUEST_URI'];
+
+		} elseif (isset($_SERVER['PHP_SELF'])) {
+			$input = $_SERVER['PHP_SELF'];
+
+		} elseif (isset($_SERVER['SCRIPT_NAME'])) {
+			$input = $_SERVER['SCRIPT_NAME'];
+
+		} elseif (isset($_SERVER['SCRIPT_FILENAME'])) {
+			$input = $_SERVER['SCRIPT_FILENAME'];
+		}
+
+		if (!empty($extra)) {
+			$input .= $extra;
+		}
+		return substr(md5($input), 8, 16);
 	}
 }
