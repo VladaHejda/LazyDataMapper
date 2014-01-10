fb7d68ea4977ce24cb5236223ac177de5b4f4ca3
VladaHejda <hejda@vspdata.cz>
2014-01-10 11:30:57 +0100

    DataHolder get/set

--- Shelter/DataHolder.php
+++ Shelter/DataHolder.php
@@ -5,31 +5,58 @@ namespace Shelter;
 class DataHolder implements IDataHolder
 {
 
+	/** @var array */
+	protected $params = array();
+
+	/** @var ISuggestor */
+	protected $suggestor;
+
+	/** @var array */
+	protected $ids;
+
+	/** @var bool */
+	protected $isContainer;
+
+
 	/**
 	 * @param ISuggestor $suggestor
 	 * @param array $ids for container holder
 	 */
 	public function __construct(ISuggestor $suggestor, array $ids = NULL)
 	{
-	}
-
-
-	/**
-	 * @experimental
-	 * Strict mode accepts only setting suggested parameters.
-	 * Otherwise accepts to set all parameters from map independent on what has been suggested.
-	 */
-	public function setStrictMode($on = TRUE)
-	{
+		$this->suggestor = $suggestor;
+		$this->ids = $ids;
+		$this->isContainer = NULL !== $ids;
 	}
 
 
 	/**
 	 * @param array|array[] $params array for one; array of arrays for container, indexed by id
 	 * @return void
+	 * @throws Exception on not suggested/unknown parameter
+	 * @throws Exception on unknown id
 	 */
 	public function setParams(array $params)
 	{
+		$suggestions = array_fill_keys($this->suggestor->getParamNames(), TRUE);
+		if ($this->isContainer) {
+			if ($diff = array_diff(array_keys($params), $this->ids)) {
+				if (is_int(current($diff))){
+					throw new Exception("Invalid ids: " . implode(', ', $diff) . ".");
+				}
+				throw new Exception("You must set parameters for each id via two-dimensional array.");
+			}
+			foreach ($params as $id => $plainParams) {
+				$this->checkAgainstSuggestions(array_keys($plainParams), $suggestions);
+				if (!isset($this->params[$id])) {
+					$this->params[$id] = array();
+				}
+				$this->params[$id] = $plainParams + $this->params[$id];
+			}
+		} else {
+			$this->checkAgainstSuggestions(array_keys($params), $suggestions);
+			$this->params = $params + $this->params;
+		}
 	}
 
 
@@ -39,6 +66,19 @@ class DataHolder implements IDataHolder
 	 */
 	public function getParams($type = NULL)
 	{
+		if (NULL === $type) {
+			return $this->params;
+		}
+
+		$map = $this->suggestor->getParamMap()->getMap($type);
+		foreach ($map as $paramName => & $value) {
+			if (array_key_exists($paramName, $this->params)) {
+				$value = $this->params[$paramName];
+			} else {
+				unset ($map[$paramName]);
+			}
+		}
+		return $map;
 	}
 
 
@@ -66,6 +106,7 @@ class DataHolder implements IDataHolder
 	 */
 	public function getSuggestor()
 	{
+		return $this->suggestor;
 	}
 
 
@@ -91,5 +132,15 @@ class DataHolder implements IDataHolder
 
 	public function next()
 	{
+	}
+
+
+	private function checkAgainstSuggestions(array $paramNames, array $suggestions)
+	{
+		foreach ($paramNames as $paramName) {
+			if (!isset($suggestions[$paramName])) {
+				throw new Exception("Parameter $paramName is unknown or is not suggested.");
+			}
+		}
 	}
 }
--- tests/DataHolder.GetSetTest.php
+++ tests/DataHolder.GetSetTest.php
@@ -10,11 +10,25 @@ class GetSetTest extends Shelter\Tests\TestCase
 	/** @var \Mockery\Mock */
 	private $suggestor;
 
+	/** @var \Mockery\Mock */
+	private $paramMap;
+
 
 	public function setUp()
 	{
 		parent::setUp();
-		$this->suggestor = \Mockery::mock('Shelter\ISuggestor');
+		$this->paramMap = \Mockery::mock('Shelter\IParamMap');
+		$this->suggestor = \Mockery::mock('Shelter\ISuggestor')
+			->shouldReceive('getParamMap')
+			->andReturn($this->paramMap)
+			->getMock();
+	}
+
+
+	public function testGetParamsNotSet()
+	{
+		$holder = new Shelter\DataHolder($this->suggestor);
+		$this->assertEquals(array(), $holder->getParams());
 	}
 
 
@@ -22,6 +36,41 @@ class GetSetTest extends Shelter\Tests\TestCase
 	{
 		$holder = new Shelter\DataHolder($this->suggestor);
 
-		$this->assertEquals(array(), $holder->getParams());
+		$this->suggestor
+			->shouldReceive('getParamNames')
+			->once()
+			->andReturn(array('name'));
+
+		$params = array('name' => 'Marry');
+		$holder->setParams($params);
+		$this->assertEquals($params, $holder->getParams());
+	}
+
+
+	public function testSetParamsTyped()
+	{
+		$holder = new Shelter\DataHolder($this->suggestor);
+
+		$this->paramMap
+			->shouldReceive('getMap')
+			->once()
+			->with('personal')
+			->andReturn(array('name' => NULL, 'age' => NULL))
+			->getMock()
+			->shouldReceive('getMap')
+			->once()
+			->with('skills')
+			->andReturn(array('power' => NULL, 'intelligence' => NULL));
+
+		$this->suggestor
+			->shouldReceive('getParamNames')
+			->once()
+			->andReturn(array('name', 'age', 'power'));
+
+		$params = array('name' => 'Marry', 'power' => 250);
+		$holder->setParams($params);
+		$this->assertEquals($params, $holder->getParams());
+		$this->assertEquals(array('name' => 'Marry'), $holder->getParams('personal'));
+		$this->assertEquals(array('power' => 250), $holder->getParams('skills'));
 	}
 }
