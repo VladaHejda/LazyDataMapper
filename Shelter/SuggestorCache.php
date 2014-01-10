47931627327caef915cc28c1062dc3b5e5f6915b
VladaHejda <hejda@vspdata.cz>
2014-01-10 16:24:45 +0100

    wip

--- Shelter/SuggestorCache.php
+++ Shelter/SuggestorCache.php
@@ -5,12 +5,21 @@ namespace Shelter;
 class SuggestorCache implements ISuggestorCache
 {
 
+	/** @var IExternalCache */
+	protected $externalCache;
+
+	/** @var string */
+	protected $key;
+
+
 	/**
 	 * @param IExternalCache $cache
 	 * @param IRequestKey $requestKey
 	 */
 	public function __construct(IExternalCache $cache, IRequestKey $requestKey)
 	{
+		$this->externalCache = $cache;
+		$this->key = $requestKey->getKey() . ':';
 	}
 
 
@@ -22,6 +31,20 @@ class SuggestorCache implements ISuggestorCache
 	 */
 	public function cacheParamName($identifier, $paramName, IParamMap $map)
 	{
+		$key = $this->key . $identifier;
+		$cached = $this->externalCache->load($key);
+		if (NULL === $cached) {
+			$cached = array();
+		}
+
+		if (!isset($cached[self::PARAM_NAMES])) {
+			$cached[self::PARAM_NAMES] = array();
+		}
+		if (!in_array($paramName, $cached[self::PARAM_NAMES])) {
+			$cached[self::PARAM_NAMES][] = $paramName;
+			$this->externalCache->save($key, $cached);
+		}
+		return $this->createSuggestor($map, $identifier, array($paramName));
 	}
 
 
@@ -33,17 +56,36 @@ class SuggestorCache implements ISuggestorCache
 	 */
 	public function cacheDescendant($identifier, $descendantEntityClass, $sourceParam = NULL)
 	{
+		$key = $this->key . $identifier;
+		$cached = $this->externalCache->load($key);
+		if (NULL === $cached) {
+			$cached = array();
+		}
+		if (!isset($cached[self::DESCENDANTS])) {
+			$cached[self::DESCENDANTS] = array();
+		}
+
+		if (!isset($cached[self::DESCENDANTS][$descendantEntityClass])) {
+			$cached[self::DESCENDANTS][$descendantEntityClass] = $sourceParam;
+			$this->externalCache->save($key, $cached);
+		}
 	}
 
 
 	/**
-	 * @todo identifier would be given in object.. ?
 	 * @param string $identifier
 	 * @param IParamMap $map
 	 * @return ISuggestor
 	 */
 	public function getCached($identifier, IParamMap $map)
 	{
+		$cached = $this->getCached($this->key . $identifier, $map);
+		if (NULL === $cached) {
+			return NULL;
+		}
+		$suggestions = isset($cached[self::PARAM_NAMES]) ? $cached[self::PARAM_NAMES] : array();
+		$descendants = isset($cached[self::DESCENDANTS]) ? $cached[self::DESCENDANTS] : array();
+		return $this->createSuggestor($map, $identifier, $suggestions, $descendants);
 	}
 
 
@@ -56,5 +98,6 @@ class SuggestorCache implements ISuggestorCache
 	 */
 	protected function createSuggestor(IParamMap $paramMap, $identifier, array $suggestions, array $descendants = array())
 	{
+		// todo: return new Suggestor($paramMap, $this, $suggestions, $identifier, , $descendants);
 	}
 }
