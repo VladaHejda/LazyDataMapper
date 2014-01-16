d0f35390e6d0e5cd935dfdcecec1cf74c8e47f5c
VladaHejda <hejda@vspdata.cz>
2014-01-16 14:12:26 +0100

    ParamMap

--- Shelter/ParamMap.php
+++ Shelter/ParamMap.php
@@ -8,11 +8,102 @@ abstract class ParamMap implements IParamMap
 	/** @var array set this map in descendant */
 	protected $map = array();
 
+	/** @var bool */
+	private $separatedByType;
+
+
+	public function __construct()
+	{
+		foreach ($this->map as $map) {
+			if (NULL === $this->separatedByType) {
+				$this->separatedByType = is_array($map);
+				continue;
+			}
+
+			if ($this->separatedByType && !is_array($map)) {
+				throw new Exception(get_class($this).": map is defective.");
+			}
+		}
+	}
+
 
 	/**
+	 * @param string $type
+	 * @param bool $flip
 	 * @return array
+	 * @throws Exception
 	 */
-	public function getMap()
+	public function getMap($type = NULL, $flip = TRUE)
 	{
+		if (NULl !== $type) {
+			if ($flip) {
+				return $this->map;
+			}
+			if (!$this->separatedByType) {
+				return array_fill_keys($this->map, NULL);
+			}
+			$types = $this->map;
+			foreach ($types as &$map) {
+				$map = array_fill_keys($map, NULL);
+			}
+			return $types;
+
+		} else {
+			if (!$this->separatedByType) {
+				throw new Exception(get_class($this).": map is not separated by type, nevertheless type $type required.");
+			}
+			if (!isset($this->map[$type])) {
+				throw new Exception(get_class($this).": unknown type $type.");
+			}
+
+			if ($flip) {
+				return $this->map[$type];
+			}
+			return array_fill_keys($this->map[$type], NULL);
+		}
+	}
+
+
+	/**
+	 * @return bool
+	 */
+	public function isSeparatedByType()
+	{
+		return $this->separatedByType;
+	}
+
+
+	/**
+	 * @param string $type
+	 * @return bool
+	 * @throws Exception
+	 */
+	public function hasType($type)
+	{
+		if (!$this->separatedByType) {
+			throw new Exception(get_class($this).": map is not separated by type.");
+		}
+
+		return isset($this->map[$type]);
+	}
+
+
+	/**
+	 * @param string $paramName
+	 * @return string
+	 * @throws Exception
+	 */
+	public function getParamType($paramName)
+	{
+		if (!$this->separatedByType) {
+			throw new Exception(get_class($this).": map is not separated by type.");
+		}
+
+		foreach ($this->map as $type => $map) {
+			if (in_array($paramName, $map)) {
+				return $type;
+			}
+		}
+		throw new Exception(get_class($this).": unknown parameter name $paramName.");
 	}
 }
