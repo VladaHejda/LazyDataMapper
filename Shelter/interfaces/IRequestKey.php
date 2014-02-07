cd5be3992b247b0fd7f359f326e5eaf399a46ab9
VladaHejda <hejda@vspdata.cz>
2014-02-07 07:09:50 +0100

    RequestKey checker todo

--- Shelter/interfaces/IRequestKey.php
+++ Shelter/interfaces/IRequestKey.php
@@ -2,6 +2,10 @@
 
 namespace Shelter;
 
+/**
+ * @todo some analyzer (etc?) to check that requests are not redundant
+ * (e.g. addresses like "product/detail/123?whatever" and ""product/detail/456" have to have equal key)
+ */
 interface IRequestKey
 {
 
